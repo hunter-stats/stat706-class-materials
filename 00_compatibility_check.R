724ce2d318568365c35ea9e8221041c5a581905c
Vitaly Druker <vdruker@gmail.com>
2020-08-24 15:33:51 -0400

    class 1 first draft

---
+++ 00_compatibility_check.R
@@ -0,0 +1,6 @@
+library(tidyverse)
+library(faraway)
+library(knitr)
+
+data(debt)
+nrow(debt)
---
+++ intro-to-R.R
@@ -0,0 +1,20 @@
+# These are comments
+
+# R/IDE Overview
+# load faraway package (install if missing with install.packages)
+
+# load gavote data
+
+# look at help for gavote
+
+# look at the summary
+
+# add a new variable (undercount (see page 3 of book))
+
+# histogram of undercount (load ggplot2)
+# density of undercount with rug
+
+# Frequencies of equipment ( `equip`)
+# percent gore (gore/votes)
+
+# Convert this to an R Markdown file
