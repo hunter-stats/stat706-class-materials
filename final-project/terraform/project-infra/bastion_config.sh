e0016304452ba29a41f87c618446062d09ec42bc
Nickhil Sethi <ns1635@nyu.edu>
2020-11-15 15:45:39 -0500

    adding clone step to provision

--- final-project/terraform/project-infra/bastion_config.sh
+++ final-project/terraform/project-infra/bastion_config.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
-sudo yum install -y postgresql git python3 python3-wheel python-pip
+# install system deps
+sudo yum install -y postgresql git python3 python3-wheel python-pip gcc
+
+# install pip deps
 pip install kaggle
 pip install pandas
-pip install psycopg2
+pip install psycopg2-binary
+
+# write kaggle creds
 mkdir /home/ec2-user/.kaggle
 touch /home/ec2-user/.kaggle/kaggle.json
 echo ${kaggle_credentials} > /home/ec2-user/.kaggle/kaggle.json
+
+# download data
+kaggle datasets download -d rounakbanik/the-movies-dataset
+unzip the-movies-dataset.zip
+
+# clone the repo
+git clone https://github.com/Nickhil-Sethi/stat706-class-materials.git
