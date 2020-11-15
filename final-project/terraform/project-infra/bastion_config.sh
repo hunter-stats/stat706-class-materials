f79cd5e75a30c90f1bae210441d0a90085ead0b8
Nickhil Sethi <ns1635@nyu.edu>
2020-11-15 14:53:27 -0500

    more installs

--- final-project/terraform/project-infra/bastion_config.sh
+++ final-project/terraform/project-infra/bastion_config.sh
@@ -2,6 +2,8 @@
 
 sudo yum install -y postgresql git python3 python3-wheel python-pip
 pip install kaggle
+pip install pandas
+pip install psycopg2
 mkdir /home/ec2-user/.kaggle
 touch /home/ec2-user/.kaggle/kaggle.json
 echo ${kaggle_credentials} > /home/ec2-user/.kaggle/kaggle.json
