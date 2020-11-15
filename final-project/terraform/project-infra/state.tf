ca756a91075669de4f4571d6ff72dfdc9fdafc65
Nickhil Sethi <ns1635@nyu.edu>
2020-11-15 13:44:32 -0500

    tf remote state file

---
+++ final-project/terraform/project-infra/state.tf
@@ -0,0 +1,10 @@
+terraform {
+  backend "s3" {
+    bucket = "stat706-final-project-tf-state"
+    key = "global/s3/terraform.tfstate"
+    region = "us-west-2"
+
+    dynamodb_table = "stat706-final-project-tf-lock"
+    encrypt = true
+  }
+}
