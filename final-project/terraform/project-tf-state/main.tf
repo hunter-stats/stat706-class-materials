6dd99a145eb3e0aef6c1b46e705c9164304e7be2
Nickhil Sethi <ns1635@nyu.edu>
2020-11-15 13:38:37 -0500

    terraform project state artifacts

---
+++ final-project/terraform/project-tf-state/main.tf
@@ -0,0 +1,35 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "stat706-final-project-tf-state"
+
+  lifecycle {
+    prevent_destroy = true
+  }
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+}
+
+resource "aws_dynamodb_table" "terraform_lock" {
+  name = "stat706-final-project-tf-lock"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
+
